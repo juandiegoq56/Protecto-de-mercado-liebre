27cfd4024ec790a41b9e3b9c3207c99462cb6c7a
juandiegoq56 <juandiegoq56@gmail.com>
2021-12-29 23:32:35 -0500

    adicione profile

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Port=process.env.Port ||3000;
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 app.get('/',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/home.html')));
@@ -10,4 +11,4 @@ app.get('/register',(req,rest) => rest.sendFile(path.resolve(__dirname, './views
 app.get('/empty',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/empty.html')));
 app.get('/mensaje',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/mensaje.html')));
 app.get('/login',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/login.html')));
-app.listen(3000, () => console.log("Servidor corriendo en el puerto: 3000"));
+app.listen(Port, () => console.log("Servidor corriendo en el puerto: ${Port}"));
